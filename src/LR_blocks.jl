c9d82cc17e37963343629ae1c65ce91ad927f125
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-05-04 05:39:50 -0400

    new util

--- src/LR_blocks.jl
+++ src/LR_blocks.jl
@@ -3,155 +3,126 @@ function block_oo(op)
     D = (op * occupied(1, 2)
          |> hf_expectation_value |> simplify_heavy)
     println("D_ij =\n")
-    (D, translate(OccupiedOrbital => 1:2))
+    D
 end
 
 function block_ov(op)
     D = (op * occupied(1) * virtual(2)
          |> hf_expectation_value |> simplify_heavy)
     println("D_ia =\n")
-    (D, translate(
-        OccupiedOrbital => 1,
-        VirtualOrbital => 2))
+    D
 end
 
 function block_vo(op)
     D = (op * occupied(2) * virtual(1)
          |> hf_expectation_value |> simplify_heavy)
     println("D_ai =\n")
-    (D, translate(
-        OccupiedOrbital => 2,
-        VirtualOrbital => 1))
+    D
 end
 
 function block_vv(op)
     D = (op * virtual(1, 2)
          |> hf_expectation_value |> simplify_heavy)
     println("D_ab =\n")
-    (D, translate(
-        VirtualOrbital => 1:2))
+    D
 end
 
 function block_oooo(op)
     D = (op * occupied(1, 2, 3, 4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_ijkl =\n")
-    (D, translate(OccupiedOrbital => 1:4))
+    D
 end
 
 function block_ooov(op)
     D = (op * occupied(1, 2, 3) * virtual(4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_ijka =\n")
-    (D, translate(
-        OccupiedOrbital => 1:3,
-        VirtualOrbital => 4))
+    D
 end
 
 function block_oovo(op)
     D = (op * occupied(1, 2, 4) * virtual(3)
          |> hf_expectation_value |> simplify_heavy)
     println("d_ijak =\n")
-    (D, translate(
-        OccupiedOrbital => [1, 2, 4],
-        VirtualOrbital => 3))
+    D
 end
 
 function block_oovv(op)
     D = (op * occupied(1, 2) * virtual(3, 4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_ijab =\n")
-    (D, translate(
-        OccupiedOrbital => 1:2,
-        VirtualOrbital => 3:4))
+    D
 end
 
 function block_ovoo(op)
     D = (op * occupied(1, 3, 4) * virtual(2)
          |> hf_expectation_value |> simplify_heavy)
     println("d_iajk =\n")
-    (D, translate(
-        OccupiedOrbital => [1, 3, 4],
-        VirtualOrbital => 2))
+    D
 end
 
 function block_ovov(op)
     D = (op * occupied(1, 3) * virtual(2, 4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_iajb =\n")
-    (D, translate(
-        OccupiedOrbital => [1, 3],
-        VirtualOrbital => [2, 4]))
+    D
 end
 
 function block_ovvo(op)
     D = (op * occupied(1, 4) * virtual(2, 3)
          |> hf_expectation_value |> simplify_heavy)
     println("d_iabj =\n")
-    (D, translate(
-        OccupiedOrbital => [1, 4],
-        VirtualOrbital => [2, 3]))
+    D
 end
 
 function block_ovvv(op)
     D = (op * occupied(1) * virtual(2, 3, 4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_iabc =\n")
-    (D, translate(
-        OccupiedOrbital => 1,
-        VirtualOrbital => [2, 3, 4]))
+    D
 end
 
 function block_vooo(op)
     D = (op * occupied(2, 3, 4) * virtual(1)
          |> hf_expectation_value |> simplify_heavy)
     println("d_aijk =\n")
-    (D, translate(
-        OccupiedOrbital => 2:4,
-        VirtualOrbital => 1))
+    D
 end
 
 function block_voov(op)
     D = (op * occupied(2, 3) * virtual(1, 4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_aijb =\n")
-    (D, translate(
-        OccupiedOrbital => [2, 3],
-        VirtualOrbital => [1, 4]))
+    D
 end
 
 function block_vovo(op)
     D = (op * occupied(2, 4) * virtual(1, 3)
          |> hf_expectation_value |> simplify_heavy)
     println("d_aibj =\n")
-    (D, translate(
-        OccupiedOrbital => [2, 4],
-        VirtualOrbital => [1, 3]))
+    D
 end
 
 function block_vovv(op)
     D = (op * occupied(2) * virtual(1, 3, 4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_aibc =\n")
-    (D, translate(
-        OccupiedOrbital => 2,
-        VirtualOrbital => [1, 3, 4]))
+    D
 end
 
 function block_vvoo(op)
     D = (op * occupied(3, 4) * virtual(1, 2)
          |> hf_expectation_value |> simplify_heavy)
     println("d_abij =\n")
-    (D, translate(
-        OccupiedOrbital => 3:4,
-        VirtualOrbital => 1:2))
+    D
 end
 
 function block_vvvv(op)
     D = (op * virtual(1, 2, 3, 4)
          |> hf_expectation_value |> simplify_heavy)
     println("d_abcd =\n")
-    (D, translate(VirtualOrbital => 1:4))
+    D
 end
 
 function one_electron_blocks(op_func)
@@ -176,14 +147,22 @@ end
 
 function ref_ref(op)
     function f(block)
-        block(simplify(bch(op, T, 3)))
+        x = block(simplify(bch(op, T, 3)))
+        x = look_for_tensor_replacements_smart(x, make_exchange_transformer("t", "u"))
+        x = look_for_tensor_replacements_smart(x, make_exchange_transformer("s", "v"))
+        x
     end
 end
 
 function mu_ref(op, mu_ph)
     op = L_mu[mu_ph+1] * simplify(bch(op, T, 3))
     function f(block)
-        block(op)
+        x = block(op)
+        x = look_for_tensor_replacements_smart(x, make_exchange_transformer("t", "u"))
+        x = look_for_tensor_replacements_smart(x, make_exchange_transformer("s", "v"))
+        x = look_for_tensor_replacements_smart(x, make_exchange_transformer("Lt", "Lu"))
+        x = look_for_tensor_replacements_smart(x, make_exchange_transformer("Ls", "Lv"))
+        x
     end
 end
 
@@ -193,3 +172,16 @@ function mu_nu(op, mu_ph, nu_ph)
         block(op)
     end
 end
+
+function full_gs_density(op, block)
+    ref_ref(op)(block) + mu_ref(op, 0)(block) + mu_ref(op, 1)(block)
+end
+
+latex_rep = Dict([
+    "Lt" => "\\bar{t}",
+    "Ls" => "\\bar{s}",
+    "Lu" => "\\bar{u}",
+    "Lv" => "\\bar{v}",
+    "Ls1" => "\\bar{\\gamma}",
+    "γ₁" => "\\gamma"
+])
